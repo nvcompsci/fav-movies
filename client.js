9d4182a3ff2ecf17578e46593f66b0071bbe8cbc
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-11-30 14:12:30 -0500

    started fake data

--- client.js
+++ client.js
@@ -1,1 +1,45 @@
-document.querySelector("p").style.color = "red"
+console.log(loadData())
+
+function loadData() {
+    //JSON - Javascript Object Notation
+    const movies = [
+    
+    {
+        //key : value
+        "name" : "Star Wars Episode IV",
+        "genre" : "Sci-Fi",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "John Word",
+                    "gender" : "male",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 2,
+                "like" : false,
+                "user" : {
+                    "name" : "Josephine Orelia",
+                    "gender" : "female",
+                    "region" : "France"
+                }
+            }
+        ]
+    },
+
+    {
+        //key : value
+        "name" : "The Notebook",
+        "genre" : "Romantic Comedy",
+        "reviews" : []
+    }
+
+    ]
+
+    return {
+        "movies" : movies
+    }
+}
